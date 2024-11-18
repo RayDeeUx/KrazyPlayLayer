14b5e6f744ccf38066c7d8e1019c2df1efc021e9
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-17 19:41:14 -0500

    this wont go swimmingly

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -9,23 +9,23 @@ class $modify(MyGameObject, GameObject) {
 	static void onModify(auto& self) {
 		(void) self.setHookPriority("GameObject::activateObject", PREFERRED_HOOK_PRIO);
 	}
-	void activateObject() {
-		if (!Utils::modEnabled() || LevelEditorLayer::get()) return this->GameObject::activateObject();
+	virtual void activateObject() {
+		if (!Utils::modEnabled() || LevelEditorLayer::get()) return GameObject::activateObject();
 		if (this->m_objectID == 1520 && Utils::getBool("disableShakeTrigger")) return;
 		std::string noEffects = Utils::getString("noEffects");
 		if (noEffects == "Ignore") return this->activateObject();
 		if (noEffects == "Enable on Visible, Disable on Invis.") {
-			this->m_hasNoEffects = !this->m_isHide && !this->m_isInvisible && this->GameObject::getOpacity() > 0;
+			this->m_hasNoEffects = !this->m_isHide && !this->m_isInvisible && GameObject::getOpacity() > 0;
 		} else if (noEffects == "Disable on Visible, Enable on Invis.") {
-			this->m_hasNoEffects = !(!this->m_isHide && !this->m_isInvisible && this->GameObject::getOpacity() > 0);
+			this->m_hasNoEffects = !(!this->m_isHide && !this->m_isInvisible && GameObject::getOpacity() > 0);
 		} else if (utils::string::endsWith(noEffects, " (+ Ignore Invis.)")) {
 			// if "(+ Ignore Invis.)" is found at the end, need to perform invis check
-			bool isInvis = this->m_isHide || this->m_isInvisible || this->GameObject::getOpacity() <= 0;
+			bool isInvis = this->m_isHide || this->m_isInvisible || GameObject::getOpacity() <= 0;
 			if (!isInvis) this->m_hasNoEffects = utils::string::startsWith(noEffects, "Force Enable ");
 		} else {
 			// brute force override
 			this->m_hasNoEffects = noEffects == "Force Enable";
 		}
-		this->GameObject::activateObject();
+		GameObject::activateObject();
 	}
 };
