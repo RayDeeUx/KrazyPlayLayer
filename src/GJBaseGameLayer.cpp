39639f935d481ba40408459782520c98993fa2f0
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-07-19 22:22:07 -0400

    almost forgot!

--- src/GJBaseGameLayer.cpp
+++ src/GJBaseGameLayer.cpp
@@ -5,9 +5,9 @@ using namespace geode::prelude;
 
 class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
 	void shakeCamera(float duration, float strength, float interval) {
-		if (LevelEditorLayer::get()) return GJBaseGameLayer::shakeCamera(duration, strength, interval);
+		if (LevelEditorLayer::get() || !m_level) return GJBaseGameLayer::shakeCamera(duration, strength, interval);
 		const PlayLayer* pl = PlayLayer::get();
-		if (const Manager* manager = Manager::getSharedInstance(); !m_isPracticeMode && manager->noEndScreenShake && manager->isInShowComplete && pl->m_levelEndAnimationStarted) return;
+		if (const Manager* manager = Manager::getSharedInstance(); !m_level->isPlatformer() && !m_isPracticeMode && Utils::modEnabled() && manager->noEndScreenShake && manager->isInShowComplete && pl->m_levelEndAnimationStarted) return;
 		GJBaseGameLayer::shakeCamera(duration, strength, interval);
 	}
 };
